2d3eaae44fc5be6c859503b7170bf32ee4267021
Giulio-Marra <giuliomarramail@gmail.com>
2024-05-24 12:02:21 +0200

    metodi
    implementazione metodi abbassa e alza volume

--- si1-l5-be-esame/src/entities/ElementoRiproducibile.java
+++ si1-l5-be-esame/src/entities/ElementoRiproducibile.java
@@ -3,19 +3,19 @@ package entities;
 public class ElementoRiproducibile extends ElementoMultimediale {
 
     public int volume;
-    public int durata;
+    public double durata;
     public boolean player;
 
-    public ElementoRiproducibile(String nome, int volume, int durata, boolean player) {
+    public ElementoRiproducibile(String nome, double durata, boolean player) {
         super(nome);
         this.volume = 4;
         this.durata = durata;
-        this.player = true;
+        this.player = player;
     }
 
     public void play() {
         if (player) {
-            for (int i = 0; i < this.volume; i++) {
+            for (int i = 1; i <= this.volume; i++) {
                 String puntoEsclamativo = "!".repeat(i);
                 System.out.println(nome + puntoEsclamativo);
             }
@@ -23,12 +23,16 @@ public class ElementoRiproducibile extends ElementoMultimediale {
     }
 
 
-    public void abbassaVolume() {
-        
+    public void abbassaVolume(boolean abbassa) {
+        if (abbassa) {
+            this.volume--;
+        }
     }
 
-    public void alzaVolume() {
-
+    public void alzaVolume(boolean alza) {
+        if (alza) {
+            this.volume++;
+        }
     }
 
 }
